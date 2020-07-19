34aba30b4f851c54d1f72ae64b394b910d02c95e
BuildTools <unconfigured@null.spigotmc.org>
2020-07-19 09:50:56 -0400

    Gets maximum number of questions per topic

--- util/getcategories.js
+++ util/getcategories.js
@@ -1,32 +1,42 @@
-const https = require('https');
 const fs = require('fs');
+const axios = require('axios');
 
-let categories = '';
-https.get('https://opentdb.com/api_category.php', (res) => {
-    res.on('data', d => {
-        categories += d;
-    });
-    res.on('end', function () {
-        //Removes any the "Entertainment: " from "Entertainment: Television" in all applicable categories
-        categories = JSON.parse(categories);
-        for (let i = 0; i < categories.trivia_categories.length; i++) {
-            const currentCat = categories.trivia_categories[i].name;
-            for (let j = 0; j < currentCat.length; j++) {
-                if (currentCat[j] === ':') {
-                    categories.trivia_categories[i].name = currentCat.substr(j+2);
-                    break;
-                };
-            };
-        };
+async function getData(url) {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function doThings() {
+    let categories = await getData('https://opentdb.com/api_category.php');
+    for (let i = 0; i < categories['trivia_categories'].length; i++) {
+        const currentCat = categories['trivia_categories'][i].name;
+        for (let j = 0; j < currentCat.length; j++) {
+            if (currentCat[j] === ':') {
+                categories['trivia_categories'][i].name = currentCat.substr(j + 2);
+                break;
+            }
+        }
+    }
+    let qAmounts = await getData('https://opentdb.com/api_count_global.php');
 
-        fs.writeFile('../trivia/categories.json', JSON.stringify(categories), err => {
-            if (err) {
-                console.log('Error writing file', err);
-            } else {
-                console.log("Successfully wrote to file");
-            };
-        });
+    categories['trivia_categories'].forEach(cat => {
+        cat['totalQuestions'] = qAmounts['categories'][cat.id]['total_num_of_questions'];
     });
-}).on('error', e => {
-    console.error(e);
+
+    categories['trivia_categories'].push({"name": "All", 'totalQuestions': qAmounts['overall']['total_num_of_questions']});
+
+    fs.writeFile('../trivia/info/categories.json', JSON.stringify(categories), err => {
+        if (err) {
+            console.log('Error writing file', err);
+        } else {
+            console.log("Successfully wrote to file");
+        }
+    });
+}
+
+doThings().catch(e => {
+    console.log(e);
 });
